70488750c198ce763d6c2307cdb9733d472e7e8f
bunyod <bunyodbek9690@gmail.com>
2026-02-24 16:48:31 +0900

    s

--- Second_week_project/scripts/model_trainig.py
+++ Second_week_project/scripts/model_trainig.py
@@ -5,13 +5,14 @@ import joblib
 from sklearn.model_selection import train_test_split
 
 sys.path.append(r"C:\Users\bunyo\OneDrive\Desktop\AI_Course\ModularProgramProjects\Second_week_project")
-df = pd.read_csv(r"C:\Users\bunyo\OneDrive\Desktop\AI_Course\ModularProgramProjects\first_week_project\data_sampling\over_sampled_final_data.csv")
+df = pd.read_csv(r"C:\Users\bunyo\onedrive\desktop\AI_Course\ModularProgramProjects\Second_week_project\data\sampled_final_data\sampled_final_data.csv")
+
 
 from source.model_trainig import ModelTraining
 
 
-X = df.drop("Score_class", axis=1, errors='ignore')
-y = df["Score_class"]
+X = df.drop("Mag_Level", axis=1, errors='ignore')
+y = df["Mag_Level"]
 
 result = ModelTraining(X=X,y=y)
 
@@ -27,8 +28,8 @@ eng_zor_natija = jadval.iloc[0]["accuracy"]
 eng_yomon_model = jadval.iloc[-1]["Model"]
 eng_yomon_natija = jadval.iloc[-1]["accuracy"]
 
-print(f"Eng zo'r model: {eng_yomon_model} (accuracy: {eng_yomon_natija})")
-print(f"Eng zo'r model: {eng_yomon_model} (accuracy: {eng_yomon_natija})")
+print(f"Eng zo'r model: {eng_yomon_model} (accuracy: {eng_zor_model})")
+print(f"Eng yomon model: {eng_yomon_model} (accuracy: {eng_yomon_natija})")
 
 
 best_model_object = result.trained_models[eng_zor_model]
