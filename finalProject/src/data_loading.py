6b2f92ded65b9a6de4c2c00a9f4124df7129435f
bunyod <bunyodbek9690@gmail.com>
2026-03-08 15:26:01 +0900

    s

--- finalProject/src/data_loading.py
+++ finalProject/src/data_loading.py
@@ -9,14 +9,15 @@ class data_load:
     logging.info('Data_set loading is started')
     def __init__(self, path:str, num:int):
         self.path = path
-        self.num = 4
+        self.num = num
     
    
     def load_data(self):
         try:
             df = pd.read_csv(self.path)
             logging.info(f"data_set loaded with {df.shape[0]} rows and {df.shape[1]} columns")
-            return self.df.sample(self.num)
+            print(df.sample(self.num))
+            return df
     
         except Exception as e:
             print(f'data_set not loaded {e}')
--- finalProject/src/log.py
+++ finalProject/src/log.py
@@ -20,9 +20,4 @@ def get_logger(log_file_name):
         formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
-    return logger    
-        
-    
-    
-    
-    
+    return logger 
